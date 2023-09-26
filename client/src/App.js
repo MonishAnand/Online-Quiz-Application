19ee2629f054c8d15e1ed3f30c11e0349c4c431e
monish <monishanand.j@gmail.com>
2023-09-26 17:21:04 +0530

    antd setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 
-
+import { Button, Space } from 'antd';
 function App() {
   return (
     <div className="App">
       <h1>
         online quiz application
       </h1>
+      <Button type='primary'>
+        Primary button
+      </Button>
     </div>
   );
 }
