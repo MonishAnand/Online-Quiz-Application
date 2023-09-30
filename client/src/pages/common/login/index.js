f7f3093c15b9c43b04ef002350a5205a3185d405
monish <monishanand.j@gmail.com>
2023-09-30 19:36:01 +0530

    register page

--- client/src/pages/common/login/index.js
+++ client/src/pages/common/login/index.js
@@ -1,28 +1,38 @@
 import { Form } from "antd";
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Login() {
+
+  const onFinish = (values) => {
+    console.log(values);
+  }
   return (
     <div className='flex justify-center item-center h-screen w-screen'>
       <div className="card w-400 p-3">
         <div className="flex flex-col"></div>
         <h1 className="text-2xl divider">Login</h1>
-       <Form
-       layout="vertical">
-        <Form.Item
-        name='email'
-        label='Email'
-        >
-            <input type="text"  />
-        </Form.Item>
-        <Form.Item
-        name='password'
-        label='Password'
-        >
-            <input type="password"  />
-        </Form.Item>
-        <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
-       </Form>
+        <Form
+          layout="vertical"
+          className="mt-2"
+          onFinish={onFinish}>
+          <Form.Item
+            name='email'
+            label='Email'
+          >
+            <input type="text" />
+          </Form.Item>
+          <Form.Item
+            name='password'
+            label='Password'
+          >
+            <input type="password" />
+          </Form.Item>
+          <div className="flex flex-col gap-2">
+            <button type="submit" className="primary-contained-btn mt-2 w-100">Login</button>
+            <Link to="/register" className="text-center mt-02 underline">Not a Member? Register Here</Link>
+          </div>
+        </Form>
       </div>
     </div>
   )
--- client/src/pages/common/register/index.js
+++ client/src/pages/common/register/index.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import { Form } from "antd";
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Register() {
+
+  const onFinish = (values) => {
+    console.log(values);
+  }
   return (
-    <div>
-      
+    <div className='flex justify-center item-center h-screen w-screen'>
+      <div className="card w-400 p-3">
+        <div className="flex flex-col"></div>
+        <h1 className="text-2xl divider">Register</h1>
+        <Form
+          layout="vertical"
+          className="mt-2"
+          onFinish={onFinish}>
+          <Form.Item
+            name='name'
+            label='Name'
+          >
+            <input type="text" />
+          </Form.Item>
+          <Form.Item
+            name='email'
+            label='Email'
+          >
+            <input type="text" />
+          </Form.Item>
+          <Form.Item
+            name='password'
+            label='Password'
+          >
+            <input type="password" />
+          </Form.Item>
+          <div className="flex flex-col gap-2">
+            <button type="submit" className="primary-contained-btn mt-2 w-100">Register</button>
+            <Link to="/login" className="text-center mt-02 underline">Already a Member? Login</Link>
+          </div>
+        </Form>
+      </div>
     </div>
   )
 }
