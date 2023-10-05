7f9a9eb183106acd54ea52b5a7e0fe6cff4d9ae5
monish <monishanand.j@gmail.com>
2023-10-05 21:57:13 +0530

    add exams complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,36 @@
-
-import { Button, Space } from 'antd';
-import './stylesheets/theme.css'
-import './stylesheets/alignment.css'
-import './stylesheets/textelements.css'
-import './stylesheets/custom-components.css'
-import './stylesheets/form-elements.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/common/login';
-import Register from './pages/common/register';
+import { Button } from "antd";
+import "./stylesheets/theme.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/layout.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/common/Login";
+import Register from "./pages/common/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/common/Home";
 import Exams from "./pages/admin/Exams";
+import AddEditExam from "./pages/admin/Exams/AddEditExam";
+import Loader from "./components/Loader";
+import { useSelector } from "react-redux";
+import WriteExam from "./pages/user/WriteExam";
+import UserReports from "./pages/user/UserReports";
+import AdminReports from "./pages/admin/AdminReports";
 
 function App() {
+  const { loading } = useSelector((state) => state.loader);
   return (
-    <BrowserRouter>
-      <Routes>
-        //common routes
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+    <>
+      {loading && <Loader />}
+      <BrowserRouter>
+        <Routes>
+          {/* Common Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-         //user route
-         <Route
+          {/* User Routes */}
+          <Route
             path="/"
             element={
               <ProtectedRoute>
@@ -29,9 +38,23 @@ function App() {
               </ProtectedRoute>
             }
           />
-
-          //admin routes
-
+          <Route
+            path="/user/write-exam/:id"
+            element={
+              <ProtectedRoute>
+                <WriteExam />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/reports"
+            element={
+              <ProtectedRoute>
+                <UserReports />
+              </ProtectedRoute>
+            }
+          />
+          {/* Admin Routes */}
           <Route
             path="/admin/exams"
             element={
@@ -40,7 +63,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-           <Route
+          <Route
             path="/admin/exams/add"
             element={
               <ProtectedRoute>
@@ -58,10 +81,17 @@ function App() {
             }
           />
 
-
-
-      </Routes>
-    </BrowserRouter>
+          <Route
+            path="/admin/reports"
+            element={
+              <ProtectedRoute>
+                <AdminReports />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -5,9 +5,18 @@ app.use(express.json());
 const dbConfig = require("./config/dbConfig");
 
 const usersRoute = require("./routes/usersRoute");
+app.use("/api/users", usersRoute);
+
+const examsRoute = require("./routes/examsRoute");
+app.use("/api/users", examsRoute);
+
 
 
 const port = process.env.PORT || 5000;
+
+const path = require("path");
+__dirname = path.resolve();
+
 app.listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
