99c59c0c76601f1a178bec70d45bb932ac923993
monish <monishanand.j@gmail.com>
2023-10-06 01:22:11 +0530

    first fixes

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,7 @@ import 'antd/dist/reset.css';
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
