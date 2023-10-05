2de8e716931a00777f0850556f0a0f6c170fe51c
monish <monishanand.j@gmail.com>
2023-10-06 00:30:14 +0530

    reports

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import { Button } from "antd";
 import "./stylesheets/theme.css";
-import "./stylesheets/alignments.css";
+import "./stylesheets/alignment.css";
 import "./stylesheets/textelements.css";
 import "./stylesheets/custom-components.css";
 import "./stylesheets/form-elements.css";
 import "./stylesheets/layout.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/common/Login";
-import Register from "./pages/common/Register";
+import Login from "./pages/common/login";
+import Register from "./pages/common/register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/common/Home";
 import Exams from "./pages/admin/Exams";
-import AddEditExam from "./pages/admin/Exams/AddEditExam";
+import AddEditExam from "./pages/admin/Exams/AddEditExams";
+
 import Loader from "./components/Loader";
 import { useSelector } from "react-redux";
 import WriteExam from "./pages/user/WriteExam";
